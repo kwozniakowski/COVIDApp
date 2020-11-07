0dcc04bb17872206f39ab5cb7d7fcee9c9d50bef
kwozniakowski <kwozniakowski99@gmail.com>
2020-11-07 16:13:11 +0100

    Zmiana nazw

--- app/src/main/java/com/example/covidapp/BriefActivity.java
+++ app/src/main/java/com/example/covidapp/BriefActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class InformationActivity extends AppCompatActivity {
+public class BriefActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_information);
+        setContentView(R.layout.activity_brief);
     }
 }
