7c7cf9ce9f0ce020f570657fcb2afe2c7a28ba9f
kwozniakowski <kwozniakowski99@gmail.com>
2020-11-09 17:41:40 +0100

    Dodanie czytania csv
    Czyta csv

--- app/src/main/java/com/example/covidapp/BriefActivity.java
+++ app/src/main/java/com/example/covidapp/BriefActivity.java
@@ -1,14 +1,43 @@
 package com.example.covidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import org.json.*;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import com.opencsv.CSVReader;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.List;
 
 public class BriefActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView totalInfectionsText;
+        TextView newInfectionsText;
+        TextView totalDeathsText;
+        TextView newDeathsText;
+        String[] countries;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brief);
+
+        totalInfectionsText = findViewById(R.id.totalInfectionsText);
+        newInfectionsText = findViewById(R.id.newInfectionsText);
+        totalDeathsText = findViewById(R.id.totalDeathsText);
+        newDeathsText = findViewById(R.id.newDeathsText);
+
+        InputStream inputStream = getResources().openRawResource(R.raw.covid_data);
+        CSVFile csvFile = new CSVFile(inputStream);
+        List scoreList = csvFile.read();
+
     }
+
+
 }
--- app/src/main/java/com/example/covidapp/MainActivity.java
+++ app/src/main/java/com/example/covidapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.covidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button briefActivityButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        briefActivityButton = findViewById(R.id.briefActivityButton);
+        briefActivityButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(view.getContext(), BriefActivity.class);
+                view.getContext().startActivity(intent);
+            }
+        });
     }
+
 }
