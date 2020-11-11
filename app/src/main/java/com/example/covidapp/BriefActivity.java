075c655e78cdca000c665279f99b2d5cd086f11c
RudeDuck <pikter1232@gmail.com>
2020-11-11 17:19:10 +0100

    Przeniesiono funkcje do DataHolder + zmiana formatu danych
    Przenioslem funkcje zajmujace sie danymi z BriefActivity do DataHolder
    i zmienilem format wyswietlania zarazen i zgonow na amerykanski
    (czyli np. zamiast 10000 jest 10,000)

--- app/src/main/java/com/example/covidapp/BriefActivity.java
+++ app/src/main/java/com/example/covidapp/BriefActivity.java
@@ -27,6 +27,8 @@ public class BriefActivity extends AppCompatActivity {
     TextView dateText;
 
     ArrayList<ArrayList<String[]>> listDividedByCountries;
+    ArrayList<String[]> chosenCountryList;
+    String chosenCountryName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,26 +54,29 @@ public class BriefActivity extends AppCompatActivity {
         });
 
         Intent intent = getIntent();
-        String region = intent.getStringExtra("Region");
+        chosenCountryName = intent.getStringExtra("Region");
         
         final ArrayList<String[]> scoreList = DataHolder.getScoreList();
 
-        listDividedByCountries = divideListIntoCountries(scoreList);
-        List<String> countries = updateCountryList(listDividedByCountries);
+        listDividedByCountries = DataHolder.getListDividedByCountries();
+        ArrayList<String> countries = DataHolder.getCountryNameList();
+        chosenCountryList = DataHolder.getChosenCountryList(chosenCountryName);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, countries);
         spinner.setAdapter(adapter);
 
-        if(countries.indexOf(region) >= 0) {
-            spinner.setSelection(countries.indexOf(region));
+        if(countries.indexOf(chosenCountryName) >= 0) {
+            spinner.setSelection(countries.indexOf(chosenCountryName));
         }
 
-        setTextsForCountry(spinner.getSelectedItem().toString(),scoreList,totalInfectionsText,newInfectionsText,
+        setTextsForCountry(chosenCountryName,totalInfectionsText,newInfectionsText,
                 totalDeathsText,newDeathsText,totalTestsText,newTestsText);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                setTextsForCountry(spinner.getSelectedItem().toString(),scoreList,totalInfectionsText,newInfectionsText,
+                chosenCountryName = spinner.getSelectedItem().toString();
+                chosenCountryList = DataHolder.getChosenCountryList(chosenCountryName);
+                setTextsForCountry(chosenCountryName,totalInfectionsText,newInfectionsText,
                         totalDeathsText,newDeathsText,totalTestsText,newTestsText);
             }
 
@@ -82,81 +87,25 @@ public class BriefActivity extends AppCompatActivity {
         });
     }
 
-    public void setTextsForCountry(String country, ArrayList<String[]> scoreList, TextView a,TextView b,TextView c,TextView d, TextView e, TextView f){
-        for(int i=0; i<scoreList.size(); i++)
-        {
-            if( scoreList.get(i)[2].equals(country) )
-            {
-                String latestDate = checkLatestDate(country, listDividedByCountries);
-                if(scoreList.get(i)[3].equals(latestDate))
-                {
-                    dateText.setText(latestDate);
+    public void setTextsForCountry(String country, TextView a,TextView b,TextView c,TextView d, TextView e, TextView f){
+        String[] latestRecord = chosenCountryList.get(chosenCountryList.size() - 1);
+        String latestDate = latestRecord[3];
 
-                    String aStr = removeFloatingPointFromString(scoreList.get(i)[4]);
-                    a.setText(aStr);
-                    String bStr = removeFloatingPointFromString(scoreList.get(i)[5]);
-                    b.setText("+" + bStr);
-                    String cStr = removeFloatingPointFromString(scoreList.get(i)[7]);
-                    c.setText(cStr);
-                    String dStr = removeFloatingPointFromString(scoreList.get(i)[8]);
-                    d.setText("+" + dStr);
-                    //Nasz plik nie ma danych dla nowych testow dla ostatnich dób, dlatego raczej zrezygnujemy z tego
-                    /*String eStr = removeFloatingPointFromString(scoreList.get(i)[24]);
-                    e.setText(eStr);
-                    String fStr = removeFloatingPointFromString(scoreList.get(i)[25]);
-                    f.setText("+" + fStr);*/
-                }
-            }
-        }
-    }
+        dateText.setText(latestDate);
 
-    public List<String> updateCountryList(ArrayList<ArrayList<String[]>> dividedList) {
-        List<String> countries = new ArrayList<String>();
-        countries.add(dividedList.get(0).get(0)[2]);
-        for(int countryNr = 1; countryNr < dividedList.size(); countryNr++) {
-            String countryName = dividedList.get(countryNr).get(0)[2];
-            if(countryName.equals("World")) {
-                if(!(countries.get(0).equals("World"))) {
-                    countries.add(0, countryName);
-                }
-            }
-            countries.add(dividedList.get(countryNr).get(0)[2]);
-        }
-        return countries;
-    }
-
-    // Dzieli cala liste na poszczegolne kraje: z postaci lista[nrRekordu][nrDanej]
-    // powstaje lista[nrKraju][nrRekordu][nrDanej]
-    public ArrayList<ArrayList<String[]>> divideListIntoCountries(ArrayList<String[]> scoreList) {
-        ArrayList<ArrayList<String[]>> dividedList = new ArrayList<ArrayList<String[]>>();
-        dividedList.add(new ArrayList<String[]>());
-        dividedList.get(0).add(scoreList.get(1));
-        int countryNr = 0;
-        for(int recordNr = 2; recordNr < scoreList.size(); recordNr++) {
-            String[] record = scoreList.get(recordNr);
-            String recordCountry = record[2];
-            if(recordCountry.equals(dividedList.get(countryNr).get(0)[2]) ) {
-                dividedList.get(countryNr).add(record);
-            } else {
-                dividedList.add(new ArrayList<String[]>());
-                countryNr++;
-                dividedList.get(countryNr).add(record);
-            }
-        }
-        return dividedList;
-    }
-
-    public String checkLatestDate(String country, ArrayList<ArrayList<String[]>> dividedList) {
-        ArrayList<String[]> scoreList = new ArrayList<String[]>();
-
-        for(int countryNr = 0; countryNr < dividedList.size(); countryNr++) {
-            if(dividedList.get(countryNr).get(0)[2].equals(country)) {
-                scoreList = dividedList.get(countryNr);
-                break;
-            }
-        }
-
-        return scoreList.get(scoreList.size() - 1)[3];
+        // String aStr = removeFloatingPointFromString(latestRecord[4]);
+        a.setText(latestRecord[4]);
+        // String bStr = removeFloatingPointFromString(latestRecord[5]);
+        b.setText("+" + latestRecord[5]);
+        // String cStr = removeFloatingPointFromString(latestRecord[7]);
+        c.setText(latestRecord[7]);
+        // String dStr = removeFloatingPointFromString(latestRecord[8]);
+        d.setText("+" + latestRecord[8]);
+        //Nasz plik nie ma danych dla nowych testow dla ostatnich dób, dlatego raczej zrezygnujemy z tego
+        /*String eStr = removeFloatingPointFromString(scoreList.get(i)[24]);
+        e.setText(eStr);
+        String fStr = removeFloatingPointFromString(scoreList.get(i)[25]);
+        f.setText("+" + fStr);*/
     }
 
     public String removeFloatingPointFromString(String text) {
--- app/src/main/java/com/example/covidapp/DataHolder.java
+++ app/src/main/java/com/example/covidapp/DataHolder.java
@@ -4,12 +4,143 @@ import java.util.ArrayList;
 
 public class DataHolder {
     static ArrayList<String[]> scoreList;
+    static boolean isDividedListReady = false;
+    static ArrayList<ArrayList<String[]>> listDividedByCountries = new ArrayList<ArrayList<String[]>>();
+    static boolean isCountryNameListReady = false;
+    static ArrayList<String> countryNameList = new ArrayList<String>();
+    static boolean isChosenCountryListReady = false;
+    static ArrayList<String[]> chosenCountryList = new ArrayList<String[]>();
+    static String currentlyChosenCountryName = "";
 
-    public static void setScoreList(ArrayList<String[]> data) {
-        scoreList = data;
+    public static void setScoreList(ArrayList<String[]> data) { scoreList = data; }
+    public static ArrayList<String[]> getScoreList() { return scoreList; }
+
+    // Przy zwracaniu ponizszych list, zawsze najpierw sprawdzam, czy sa one w ogole
+    // przygotowane do odczytu, jesli nie - wywoluje odpowiednia funkcje przygotowujaca
+    public static ArrayList<ArrayList<String[]>> getListDividedByCountries() {
+        if(isDividedListReady) {
+            return listDividedByCountries;
+        }
+        else {
+            divideListIntoCountries();
+            return listDividedByCountries;
+        }
     }
 
-    public static ArrayList<String[]> getScoreList() {
-        return scoreList;
+    public static ArrayList<String> getCountryNameList() {
+        if(isCountryNameListReady) {
+            return countryNameList;
+        }
+        else {
+            updateCountryNames();
+            return countryNameList;
+        }
+    }
+
+
+    // Tutaj sprawdzam tez, czy wybrany kraj sie zmienil. Jesli tak, to aktualizuje liste.
+    public static ArrayList<String[]> getChosenCountryList(String countryName) {
+        if(isChosenCountryListReady) {
+            if(countryName.equals(currentlyChosenCountryName)) {
+                return chosenCountryList;
+            }
+            else {
+                updateChosenCountryList(countryName);
+                return chosenCountryList;
+            }
+        }
+        else {
+            updateChosenCountryList(countryName);
+            return chosenCountryList;
+        }
+    }
+
+    // Ponizej funkcje przygotowujace dla list
+
+    // Dzieli cala liste na poszczegolne kraje: z postaci lista[nrRekordu][nrDanej]
+    // powstaje lista[nrKraju][nrRekordu][nrDanej]
+    public static void divideListIntoCountries() {
+        ArrayList<ArrayList<String[]>> dividedList = new ArrayList<ArrayList<String[]>>();
+        dividedList.add(new ArrayList<String[]>());
+        dividedList.get(0).add(scoreList.get(1));
+        int countryNr = 0;
+        for(int recordNr = 2; recordNr < scoreList.size(); recordNr++) {
+            String[] record = scoreList.get(recordNr);
+            String recordCountry = record[2];
+            if(!(recordCountry.equals("International"))) {
+                if (recordCountry.equals(dividedList.get(countryNr).get(0)[2])) {
+                    dividedList.get(countryNr).add(record);
+                } else {
+                    dividedList.add(new ArrayList<String[]>());
+                    countryNr++;
+                    dividedList.get(countryNr).add(record);
+                }
+            }
+        }
+        listDividedByCountries = dividedList;
+        isDividedListReady = true;
+    }
+
+    public static void updateCountryNames() {
+        if(isDividedListReady) {
+            for(int countryNr = 0; countryNr < listDividedByCountries.size(); countryNr++) {
+                String countryName = listDividedByCountries.get(countryNr).get(0)[2];
+                if(countryName.equals("World")) {
+                    if(!(countryNameList.get(0).equals("World"))) {
+                        countryNameList.add(0, countryName);
+                    }
+                }
+                else {
+                    countryNameList.add(listDividedByCountries.get(countryNr).get(0)[2]);
+                }
+            }
+            isCountryNameListReady = true;
+        }
+        else {
+            divideListIntoCountries();
+            updateCountryNames();
+        }
+    }
+
+    // Jesli podana nazwa kraju znajduje sie w listDividedByCountry,
+    // do chosenCountryList zostana wpisane wszystkie rekordy z tego kraju
+    public static void updateChosenCountryList(String newChosenCountryName) {
+        if(isDividedListReady) {
+            for(int countryNr = 0; countryNr < listDividedByCountries.size(); countryNr++) {
+                String countryName = listDividedByCountries.get(countryNr).get(0)[2];
+                if(countryName.equals(newChosenCountryName)) {
+                    chosenCountryList = listDividedByCountries.get(countryNr);
+                    removeFloatingPointsFromList();
+                    break;
+                }
+            }
+            currentlyChosenCountryName = newChosenCountryName;
+            isChosenCountryListReady = true;
+        }
+        else {
+            divideListIntoCountries();
+            updateChosenCountryList(newChosenCountryName);
+        }
+    }
+
+    public static void removeFloatingPointsFromList() {
+        for(int recordNr = 0; recordNr < chosenCountryList.size(); recordNr++) {
+            String[] record = chosenCountryList.get(recordNr);
+            chosenCountryList.get(recordNr)[4] = reformatString(record[4]);
+            chosenCountryList.get(recordNr)[5] = reformatString(record[5]);
+            chosenCountryList.get(recordNr)[7] = reformatString(record[7]);
+            chosenCountryList.get(recordNr)[8] = reformatString(record[8]);
+        }
+    }
+
+    public static String reformatString(String text) {
+        if(text.indexOf(".") >= 0) {
+            text = text.substring(0, text.indexOf("."));
+        }
+        if(text.length() > 3) {
+            int number = Integer.parseInt(text);
+            text = String.format("%,d", number);
+        }
+        return text;
     }
 }
