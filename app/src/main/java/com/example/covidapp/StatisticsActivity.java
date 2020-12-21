653a4cc58f42c1867b41aae2b2a3352f35c17f54
kwozniakowski <kwozniakowski99@gmail.com>
2020-12-21 18:46:35 +0100

    Dodanie poczatkowe wykresu
    Wykres sie robi, pewnie trzeba bedzie uporzadkowac wszystko i moze zrobic osobna klase na wykresy, ciezko powiedziec

--- app/src/main/java/com/example/covidapp/StatisticsActivity.java
+++ app/src/main/java/com/example/covidapp/StatisticsActivity.java
@@ -1,6 +1,5 @@
 package com.example.covidapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,6 +8,11 @@ import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
 import java.util.ArrayList;
 
 public class StatisticsActivity extends AppCompatActivity {
@@ -16,6 +20,7 @@ public class StatisticsActivity extends AppCompatActivity {
     Spinner infectionsSpinner;
     Spinner deathsSpinner;
     Spinner testsSpinner;
+    BarChart barChart1, barChart2, barChart3;
 
     String chosenCountryName;
 
@@ -26,6 +31,16 @@ public class StatisticsActivity extends AppCompatActivity {
 
         countrySpinner = findViewById(R.id.countrySpinner);
 
+        //Ponizej odbywa sie cala zabawa z wykresami, na razie jest to losowy kraj i losowy dzien itp,
+        //trzeba zrobic zeby dalo sie fajnie wybrac i chyba nie bedzie to trudne
+        barChart1 = findViewById(R.id.barChart1);
+        BarDataSet barDataSet1 = new BarDataSet(dataValues1(),"Label");
+        BarData barData = new BarData(barDataSet1);
+        barChart1.setData(barData);
+        barChart1.animateY(2000);
+
+        barChart2 = findViewById(R.id.barChart2);
+        barChart3 = findViewById(R.id.barChart3);
         final ArrayList<String> countryNameList = DataHolder.getCountryNameList();
         chosenCountryName = DataHolder.getChosenCountryName();
 
@@ -45,5 +60,19 @@ public class StatisticsActivity extends AppCompatActivity {
             }
         });
         //Trzeba bedzie ustawic tu liste wyboru panstwa dla spinnera ale to juz potem
+    }
+
+    private ArrayList<BarEntry> dataValues1()
+    {
+        DataHolder.getChosenCountryName();
+        ArrayList<BarEntry> list = new ArrayList<>();
+        list.add(new BarEntry(0,Float.parseFloat(DataHolder.getListDividedByCountries().get(15).get(200)[4])));
+        list.add(new BarEntry(1,Float.parseFloat(DataHolder.getListDividedByCountries().get(15).get(201)[4])));
+        list.add(new BarEntry(2,Float.parseFloat(DataHolder.getListDividedByCountries().get(15).get(202)[4])));
+        list.add(new BarEntry(3,Float.parseFloat(DataHolder.getListDividedByCountries().get(15).get(203)[4])));
+        list.add(new BarEntry(4,Float.parseFloat(DataHolder.getListDividedByCountries().get(15).get(204)[4])));
+        list.add(new BarEntry(5,Float.parseFloat(DataHolder.getListDividedByCountries().get(15).get(205)[4])));
+        return list;
+
     }
 }
