810cedd27bc67aa83f2ab6a36758e80a185af366
RudeDuck <pikter1232@gmail.com>
2020-12-28 23:41:08 +0100

    MainActivity sprawdza w necie date najnowszej wersji pliku csv

--- app/src/main/java/com/example/covidapp/MainActivity.java
+++ app/src/main/java/com/example/covidapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.covidapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,15 @@ import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,10 +31,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        countryBriefButton = (Button)findViewById(R.id.countryBriefButton);
-        worldBriefButton = (Button)findViewById(R.id.worldBriefButton);
-        createModelButton = (Button)findViewById(R.id.createModelButton);
-        settingsButton = (Button)findViewById(R.id.settingsButton);
+        countryBriefButton = (Button) findViewById(R.id.countryBriefButton);
+        worldBriefButton = (Button) findViewById(R.id.worldBriefButton);
+        createModelButton = (Button) findViewById(R.id.createModelButton);
+        settingsButton = (Button) findViewById(R.id.settingsButton);
+
+        checkForFileUpdates();
 
         InputStream inputStream = getResources().openRawResource(R.raw.covid_data);
         CSVFile csvFile = new CSVFile(inputStream);
@@ -60,5 +70,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void checkForFileUpdates() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Document doc = Jsoup.connect("https://github.com/owid/covid-19-data/commits/master/public/data/owid-covid-data.csv")
+                            .get();
+                    Element latestUpdates = doc.getElementsByClass("TimelineItem-body").get(0);
+                    String latestUpdateDate = latestUpdates.getElementsByTag("h2").get(0).text();
+                    if(latestUpdateDate.indexOf("Commits on ") == 0) {
+                        latestUpdateDate = latestUpdateDate.substring(11);
+                    }
+                    System.out.println("Najnowsza aktualizacja: " + latestUpdateDate);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 }
