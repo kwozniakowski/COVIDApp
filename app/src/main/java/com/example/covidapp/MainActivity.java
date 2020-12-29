9b7ceffe68321c83d86e736d096a3089a491e8c8
RudeDuck <pikter1232@gmail.com>
2020-12-30 00:25:04 +0100

    Pobieranie z neta do pliku id najnowszego commita zwiazanego z plikiem csv

--- app/src/main/java/com/example/covidapp/MainActivity.java
+++ app/src/main/java/com/example/covidapp/MainActivity.java
@@ -3,19 +3,31 @@ package com.example.covidapp;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 
@@ -76,21 +88,108 @@ public class MainActivity extends AppCompatActivity {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
+                // Sprawdzam jaka wersja pliku csv jest obecnie pobrana, zeby porownac ja z dostepna
+                // w internecie
+                String dataFromFile = readFromFile("covid_data_file_info.txt");
+                System.out.println("Dane odczytane z pliku: " + dataFromFile);
+
                 try {
+                    // Laczenie sie ze strona zawierajaca informacje o zmianach w pliku csv
                     Document doc = Jsoup.connect("https://github.com/owid/covid-19-data/commits/master/public/data/owid-covid-data.csv")
                             .get();
+                    // Jako odpowiedz aplikacja otrzyma zwykly dokument HTML ze wszystkimi potrzebnymi informacjami
+
+                    // Przeszukuje otrzymany dokument, zeby znalezc informacje o najnowszych commitach
+                    // Interesuje mnie glownie id commitu
                     Element latestUpdates = doc.getElementsByClass("TimelineItem-body").get(0);
+                    Element latestCommits = latestUpdates.getElementsByTag("ol").get(0);
+                    Element latestCommit = latestCommits.getElementsByTag("li").get(0);
+                    Element buttonGroup = latestCommit.getElementsByClass("BtnGroup").get(0);
+                    String latestCommitId = buttonGroup.getElementsByTag("a").get(0).text();
+
+                    // Zapisuje rowniez informacje o dacie utworzenia tego commitu
+                    // Poki co nie jest to nigdzie uzywane, ale moze sie jeszcze przydac
                     String latestUpdateDate = latestUpdates.getElementsByTag("h2").get(0).text();
                     if(latestUpdateDate.indexOf("Commits on ") == 0) {
                         latestUpdateDate = latestUpdateDate.substring(11);
                     }
+
                     System.out.println("Najnowsza aktualizacja: " + latestUpdateDate);
+                    System.out.println("Id commitu: " + latestCommitId);
+
+                    if(!(dataFromFile.equals(latestCommitId + "\n"))) {
+                        // Tu bedzie pobieranie pliku
+                        makeToast("Update available, updating...");
+                        writeToFile("covid_data_file_info.txt", latestCommitId);
+                    }
+                    else {
+                        makeToast("Everything is up to date");
+                    }
+
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    makeToast("Cannot check for updates");
                 }
             }
         };
         Thread thread = new Thread(runnable);
         thread.start();
+    }
+
+    private String readFromFile(String fileName) {
+        FileInputStream fis = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            fis = openFileInput(fileName);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String text = "";
+
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            return "";
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    private void writeToFile(String fileName, String dataToWrite) {
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(fileName, MODE_PRIVATE);
+            fos.write(dataToWrite.getBytes());
+            makeToast("Data updated!");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void makeToast(final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
